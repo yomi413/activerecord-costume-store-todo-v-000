f4be358bba2a89345d644b648a0e3656b96f3a92
Yomaira Escano <yjescano@gmail.com>
2018-01-04 03:07:00 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,10 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  create_table :haunted_houses do |t|
+    t.string :name
+    t.string :location
+    t.string :theme
+    t.float :price
+    t.boolean :family_friendly
+  end
 end
