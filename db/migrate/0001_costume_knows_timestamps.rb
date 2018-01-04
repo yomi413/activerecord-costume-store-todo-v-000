b28a3e9a7cd0f7fd32f8bd4749934564fe48de16
Yomaira Escano <yjescano@gmail.com>
2018-01-04 02:40:49 +0000

    automatically backed up by learn

--- db/migrate/0001_costume_knows_timestamps.rb
+++ db/migrate/0001_costume_knows_timestamps.rb
@@ -1,4 +1,4 @@
-class CostumeKnowsWhenCreatedAndUpdated < ActiveRecord::Migration[4.2]
+class CostumeKnowsTimestamps < ActiveRecord::Migration[4.2]
 
   def change
     add_column :costumes, :timestamps
